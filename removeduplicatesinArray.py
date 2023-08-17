ff1d739e78831d7d9adf7ef2279c4d75ea9c64e5
Parthsarthy Nandi <119975058+Parththepeter@users.noreply.github.com>
2023-08-17 01:12:12 -0500

    Add files via upload

---
+++ removeduplicatesinArray.py
@@ -0,0 +1,27 @@
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+#  Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+      nums = sorted(nums) # first sort the array without removing duplicate elements.
+      stack = []
+      for i in range(len(nums)-1):
+        if nums[i] != nums[i+1]:
+          stack.append(nums[i])
+      stack.append(nums[-1])
+      return len(stack), stack
+
+sol1 = Solution()
+
+nums = [1,1,2]
+sol1.removeDuplicates(nums)
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+sol1.removeDuplicates(nums)
+
+nums = [1,1,1,0,0,2,2,3,4,5,9,8]
+sol1.removeDuplicates(nums)
